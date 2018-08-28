b075544355bb01afdc9c38f2c21bff9a561fef94
Paul Clough <pclough@miami.edu>
2018-08-28 09:27:16 -0400

    Remove JavaScript that collapses long blocks of text

--- js/qubit.js
+++ js/qubit.js
@@ -13,23 +13,3 @@ window.log = function()
     }
   };
 
-// jQuery expander
-Drupal.behaviors.expander = {
-  attach: function (context)
-    {
-      jQuery('div.field:not(:has(div.field)) > div').each(function (index, element) {
-        var $element = jQuery(element);
-        // Don't apply expander to fields with only one child, if that child is a list
-        if ($element.children().length !== 1 || !$element.children().first().is('ul')) {
-          $element.expander({
-            slicePoint: 255,
-            expandText: '&raquo;',
-            expandPrefix: '... ',
-            userCollapseText: '&laquo;',
-            widow: 4,
-            expandEffect: 'show'
-          });
-        }
-      });
-    }
-  };
