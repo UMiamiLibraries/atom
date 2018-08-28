e6e08217a44902831e2f15f2eefa914fb2b8e0ea
Paul Clough <pclough@miami.edu>
2018-08-28 08:58:16 -0400

    Reconfigure "Physical storage location" display
    Make changes to the "Physical storage location" layout so it will more closely
    match the main column layout.

--- apps/qubit/modules/physicalobject/templates/_contextMenu.php
+++ apps/qubit/modules/physicalobject/templates/_contextMenu.php
@@ -1,27 +1,28 @@
 <section id="physical-objects">
 
-  <h4><?php echo sfConfig::get('app_ui_label_physicalobject') ?></h4>
+    <h2><?php echo sfConfig::get('app_ui_label_physicalobject') ?></h2>
 
-  <div class="content">
-    <ul>
 
-      <?php foreach ($physicalObjects as $item): ?>
-        <li>
-
-          <?php if (isset($item->type)): ?>
-            <?php echo $item->type ?>:
-          <?php endif; ?>
-
-          <?php echo link_to_if(QubitAcl::check($resource, 'update'), render_title($item), array($item, 'module' => 'physicalobject')) ?>
-
-          <?php if (isset($item->location) && $sf_user->isAuthenticated()): ?>
-            - <?php echo $item->getLocation(array('cultureFallback' => 'true')) ?>
-          <?php endif; ?>
-
-        </li>
-      <?php endforeach; ?>
-
-    </ul>
-  </div>
+    <?php foreach ($physicalObjects as $item): ?>
+        <div class="field">
+            <h3>
+                <?php if ($resource != $resource->getCollectionRoot()): ?>
+                    <?php if (isset($item->type)): ?>
+                        <?php echo $item->type ?>:
+                    <?php endif; ?>
+                <?php endif; ?>
+                <?php if (isset($item->location)): ?>
+                    <?php echo render_title($item) ?>
+                <?php endif; ?>
+            </h3>
+            <div>
+                <?php if (isset($item->location)): ?>
+                    <?php echo $item->getLocation(array('cultureFallback' => 'true')) ?>
+                <?php else: ?>
+                    <?php echo render_title($item) ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    <?php endforeach; ?>
 
 </section>
